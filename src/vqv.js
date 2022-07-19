eeaadf0e5ede160fc41fa75fbc0ba6df30effa7b
Estevao <estevo144@gmail.com>
2022-07-19 17:47:07 -0300

    questão 4 feita

--- src/vqv.js
+++ src/vqv.js
@@ -18,16 +18,16 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
 
-  const vqv = (nome,idade) => {
-    if(nome === undefined || idade === undefined){
-      return undefined;
-    }
-    return`Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
-  };
+#VQV!`;
+};
 // console.log(vqv('Tunico', 29));
 
 module.exports = vqv;
